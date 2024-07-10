35a086a525d1a5eb39b915ce37fc2ca580e038ee
mattorga <mattheworga85@gmail.com>
2024-07-10 09:12:41 +0800

    Learning with React Router

---
+++ phonebook/src/routes/root.jsx
@@ -0,0 +1,71 @@
+import { Outlet, Link, useLoaderData, Form } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+const action = async () => {
+    const contact = await createContact();
+    return { contact };
+}
+
+const loader = async () => {
+    const contacts = await getContacts();
+    return { contacts };
+}
+
+const Root = () => {
+    const { contacts } = useLoaderData();
+    return (
+        <>
+            <div id="sidebar">
+                <h1>React Router Contacts</h1>
+                <div>
+                    <form id="search-form" role="search">
+                        <input
+                            id="q"
+                            aria-label="Search contacts"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                        />
+                        <div id="search-spinner" aria-hidden hidden={true} />
+                        <div className="sr-only" aria-live="polite" />
+                    </form>
+                    <Form method="post">
+                        <button type="submit">New</button>
+                    </Form>
+                </div>
+                <nav>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No Name</i>
+                                        )}
+                                        {" "}
+                                        {contact.favorite && <span>★</span>}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
+                </nav>
+            </div>
+
+            <div id="detail"> 
+                <Outlet />
+            </div>
+        </>
+    );
+}
+
+export { action, loader }
+export default Root;
